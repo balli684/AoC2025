71ed1ab7202edd36007153a91a1152c3dfd37cbc
Niels Ballis <n.ballis@rubicon.nl>
2025-12-18 16:14:53 +0100

    Day 6 part 2 WIP

--- Day06/part2.py
+++ Day06/part2.py
@@ -6,20 +6,30 @@ def read_from_file(filename):
     return data
 
 def main():
-    data = read_from_file('input.txt')
-    results = []
-    for x in range(len(data[0])):
-        if data[-1][x] == "*":
-            result = 1
-            for y in range(len(data)-1):
-                result *= int(data[y][x])
-            results.append(result)
-        if data[-1][x] == "+":
-            result = 0
-            for y in range(len(data)-1):
-                result += int(data[y][x])
-            results.append(result)
-    print(sum(results))
+    data = read_from_file('test.txt')
+    # results = []
+    # for x in range(len(data[0])):
+    #     numbers = []
+    #     for y in range(len(data)-1):
+    numbers = []
+    x = 0
+    numbers[x] = []
+    for y in range(len(data[x])-1):
+        for char in data[x][y]:
+            print(char)
+        
+
+    #     if data[-1][x] == "*":
+    #         result = 1
+    #         for y in range(len(data)-1):
+    #             result *= int(data[y][x])
+    #         results.append(result)
+    #     if data[-1][x] == "+":
+    #         result = 0
+    #         for y in range(len(data)-1):
+    #             result += int(data[y][x])
+    #         results.append(result)
+    # print(sum(results))
         
 if __name__ == '__main__':
     main()
